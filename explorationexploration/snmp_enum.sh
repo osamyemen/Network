240bfc55bd1c33f9cc407693e64b2ff0b0415ee3
osam_coding_ds_is <osamayemen772@gmail.com>
2025-11-22 19:53:59 +0300

    Delete explorationexploration directory

--- explorationexploration/snmp_enum.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-nmap -sU -p 161 --script snmp-brute 192.168.56.104
-nmap -sU -p 161 --script snmp-win32-users 192.168.56.104
-nmap -sU -p 161 --script snmp-win32-services 192.168.56.104
-
-
-
